1a0cedc279b1a2706915cb45ad61c3fb14e08096
Lars <lars_31@hotmail.de>
2025-04-21 14:36:41 +0200

    Add key

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router'
-import paintings from "../../public/paintings.json"
 import  PaintingPreview  from '@/components/PaintingPreview';
 import { useQuery } from '@tanstack/react-query';
 import { paintingsQueryOptions } from '@/requests/requests';
@@ -20,19 +19,19 @@ export interface Painting{
 
 
 function App() {
-
   const { data, isLoading, isError, error } = useQuery(paintingsQueryOptions());
 
   return (
     <div className="text-center bg-white text-[#1e1e1e]">
       <div className="bg-white text-[#1e1e1e]">
-      {data?.map((painting: Painting) => <PaintingPreview 
+      {
+        data?.map?.((painting: Painting) => <PaintingPreview 
         title={painting.title}
         size={painting.size}
         thumbnailUrl={painting.thumbnailUrl}
         paintingUrl={painting.paintingUrl}
         description={painting.description}
-       
+        key={painting.title}       
       />)}
       
       </div>
