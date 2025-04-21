4a890cec0705ed773fe7bcd49e208ed41c3061af
Lars <lars_31@hotmail.de>
2025-04-21 14:36:11 +0200

    Fix cache usage

--- src/requests/requests.ts
+++ src/requests/requests.ts
@@ -1,43 +1,36 @@
-import { queryOptions } from "@tanstack/react-query";
+import { queryOptions } from '@tanstack/react-query'
 
 const fetchPaintings = async () => {
-    const res = await fetch(
-      "https://raw.githubusercontent.com/statist32/aforus/develop/public/paintings.json"
-    );
-    if (!res.ok) {
-      throw new Error("Failed to fetch paintings");
-    }
-    return res.json();
-  };
+  const res = await fetch(
+    'https://raw.githubusercontent.com/statist32/aforus/develop/public/paintings.json',
+  )
+  if (!res.ok) {
+    throw new Error('Failed to fetch paintings')
+  }
+  return res.json()
+}
 
-
-export const paintingsQueryOptions =() =>   queryOptions(
-    {
-        queryKey: ["paintings"],
-        queryFn: () => fetchPaintings(),
-        refetchOnMount: true
-      }
-);
-
+export const paintingsQueryOptions = () =>
+  queryOptions({
+    queryKey: ['paintings'],
+    queryFn: () => fetchPaintings(),
+    refetchOnMount: true,
+  })
 
 export const fetchPaintingBlob = async (path: string): Promise<string> => {
-    console.log("awd", path)
-    
-    const res = await fetch(
-      `https://raw.githubusercontent.com/statist32/aforus/refs/heads/develop/public${path}`
-    );
-    if (!res.ok) {
-      throw new Error("Failed to fetch image");
-    }
-    const blob = await res.blob();
-    return URL.createObjectURL(blob); // use this as <img src />
-  };
+  const res = await fetch(
+    `https://raw.githubusercontent.com/statist32/aforus/refs/heads/develop/public${path}`,
+  )
+  if (!res.ok) {
+    throw new Error('Failed to fetch image')
+  }
+  const blob = await res.blob()
+  return URL.createObjectURL(blob) // use this as <img src />
+}
 
-
-export const paintingQueryOptions = (path: string)  => queryOptions(
-    {
-        queryKey: ["paintings"],
-        queryFn: () => fetchPaintingBlob(path),
-        refetchOnMount: true
-      }
-);
+export const paintingQueryOptions = (path: string) =>
+  queryOptions({
+    queryKey: ['painting', path],
+    queryFn: () => fetchPaintingBlob(path),
+    refetchOnMount: true,
+  })
