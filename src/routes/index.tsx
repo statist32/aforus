36e1fee76bcf0c69c9455ad3b76fb87ed2ed1051
Lars <lars_31@hotmail.de>
2025-05-01 13:54:18 +0200

    Use uppercase forr links

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -18,11 +18,11 @@ function App() {
         fontSize: '3rem',
       }}
     >
-      <Link to="/paintings">Available Works</Link>
-      <Link to="https://www.twitch.tv/aforus">Twitch</Link>
-      <Link to="http://instagram.com/aforus">Instagram</Link>
-      <Link to="https://www.tiktok.com/@artbyaforus">Art TikTok</Link>
-      <Link to="https://www.tiktok.com/@af0rus">Stream TikTok</Link>
+      <Link to="/paintings">AVAILABLE WORKS</Link>
+      <Link to="https://www.twitch.tv/aforus">TWITCH</Link>
+      <Link to="http://instagram.com/aforus">INSTAGRAM</Link>
+      <Link to="https://www.tiktok.com/@artbyaforus">ART TIKTOK</Link>
+      <Link to="https://www.tiktok.com/@af0rus">STREAM TIKTOK</Link>
     </div>
   )
 }
